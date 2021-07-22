2e2bc80fc0bde817dbfeb783887ee19ae9c30984
Nail Shaykhraziev <nail.shaykhraziev@flatstack.com>
2021-07-22 18:34:25 +0300

    Add parsing links inside of block

--- android/src/main/java/com/contentful/rich/android/renderer/views/BlockRenderer.kt
+++ android/src/main/java/com/contentful/rich/android/renderer/views/BlockRenderer.kt
@@ -1,65 +1,83 @@
-package com.contentful.rich.android.renderer.views;
+package com.contentful.rich.android.renderer.views
 
-import android.text.SpannableStringBuilder;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
+import android.text.SpannableStringBuilder
+import android.text.Spanned
+import android.text.method.LinkMovementMethod
+import android.text.style.URLSpan
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.contentful.java.cda.rich.CDARichBlock
+import com.contentful.java.cda.rich.CDARichHyperLink
+import com.contentful.java.cda.rich.CDARichNode
+import com.contentful.rich.android.AndroidContext
+import com.contentful.rich.android.AndroidProcessor
+import com.contentful.rich.android.AndroidRenderer
+import com.contentful.rich.android.R
 
-import com.contentful.java.cda.rich.CDARichBlock;
-import com.contentful.java.cda.rich.CDARichNode;
-import com.contentful.rich.android.AndroidContext;
-import com.contentful.rich.android.AndroidProcessor;
-import com.contentful.rich.android.AndroidRenderer;
-import com.contentful.rich.android.R;
+open class BlockRenderer(
+    processor: AndroidProcessor<View>
+) : AndroidRenderer<AndroidContext, View>(processor) {
 
-import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
+    override fun canRender(context: AndroidContext?, node: CDARichNode): Boolean = node is CDARichBlock
 
-public class BlockRenderer extends AndroidRenderer<AndroidContext, View> {
-  public BlockRenderer(@Nonnull AndroidProcessor<View> processor) {
-    super(processor);
-  }
+    override fun render(context: AndroidContext, node: CDARichNode): View? {
+        val block = node as CDARichBlock
+        val result = inflateRichLayout(context, node)
+        val content = result.findViewById<ViewGroup>(R.id.rich_content)
+        var lastTextView: TextView? = null
+        block.content.forEach { childNode ->
+            val childView = processor.process(context, childNode!!)
+            if (childView != null) {
+                when {
+                    childView is TextView -> {
+                        lastTextView?.let {
+                            it.text = SpannableStringBuilder(it.text).append(" ").append(childView.text)
+                        } ?: run {
+                            lastTextView = childView
+                            content.addView(childView)
+                        }
+                    }
+                    childNode is CDARichHyperLink -> {
+                        val childLayout = childView.findViewById<ViewGroup>(R.id.rich_content)
+                        if (childLayout.childCount > 0) {
+                            val childTextView = childLayout.getChildAt(0) as TextView
+                            val span = URLSpan(childNode.data as String)
+                            val text = lastTextView?.let {
+                                SpannableStringBuilder(it.text).append(" ").append(childTextView.text)
+                            } ?: SpannableStringBuilder(childTextView.text)
 
-  @Override public boolean canRender(@Nullable AndroidContext context, @Nonnull CDARichNode node) {
-    return node instanceof CDARichBlock;
-  }
+                            text.setSpan(
+                                span,
+                                lastTextView?.text?.length?.plus(1) ?: 0,
+                                text.length,
+                                Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+                            )
 
-  @Nullable @Override public View render(@Nonnull AndroidContext context, @Nonnull CDARichNode node) {
-    final CDARichBlock block = (CDARichBlock) node;
-    final View result = inflateRichLayout(context, node);
-    final ViewGroup content = result.findViewById(R.id.rich_content);
-
-    TextView lastTextView = null;
-    for (final CDARichNode childNode : block.getContent()) {
-      final View childView = processor.process(context, childNode);
-
-      if (childView != null) {
-        if (childView instanceof TextView) {
-          final TextView childTextView = (TextView) childView;
-          if (lastTextView != null) {
-            lastTextView.setText(
-                new SpannableStringBuilder(lastTextView.getText()).append(childTextView.getText())
-            );
-          } else {
-            lastTextView = childTextView;
-            content.addView(childView);
-          }
-        } else {
-          if (context.getPath() != null && context.getPath().size() > 1) {
-            final View indented = context.getInflater().inflate(R.layout.rich_indention_layout, null, false);
-            ((ViewGroup) indented.findViewById(R.id.rich_content)).addView(childView);
-            content.addView(indented);
-          } else {
-            content.addView(childView);
-          }
+                            lastTextView?.let {
+                                it.movementMethod = LinkMovementMethod.getInstance()
+                                it.text = text
+                            } ?: run {
+                                childTextView.text = text
+                                lastTextView = childTextView
+                                content.addView(childView)
+                            }
+                        }
+                    }
+                    context.path?.size ?: 0 > 1 -> {
+                        val indented = context.inflater.inflate(R.layout.rich_indention_layout, null, false)
+                        (indented.findViewById<View>(R.id.rich_content) as ViewGroup).addView(childView)
+                        content.addView(indented)
+                    }
+                    else -> content.addView(childView)
+                }
+            }
         }
-      }
+        return result
     }
 
-    return result;
-  }
-
-  protected View inflateRichLayout(@Nonnull AndroidContext context, @Nonnull CDARichNode node) {
-    return context.getInflater().inflate(R.layout.rich_block_layout, null, false);
-  }
+    protected open fun inflateRichLayout(
+        context: AndroidContext,
+        node: CDARichNode
+    ): View = context.inflater.inflate(R.layout.rich_block_layout, null, false)
 }
--- android/src/main/java/com/contentful/rich/android/renderer/views/EmbeddedLinkRenderer.java
+++ android/src/main/java/com/contentful/rich/android/renderer/views/EmbeddedLinkRenderer.java
@@ -20,50 +20,54 @@ import javax.annotation.Nullable;
 import static com.contentful.rich.android.renderer.chars.EmbeddedLinkRenderer.defaultBitmapProvider;
 
 public class EmbeddedLinkRenderer extends BlockRenderer {
-  private final BitmapProvider provider;
 
-  public EmbeddedLinkRenderer(@Nonnull AndroidProcessor<View> processor) {
-    this(processor, defaultBitmapProvider);
-  }
+    private final BitmapProvider provider;
 
-  public EmbeddedLinkRenderer(@Nonnull AndroidProcessor<View> processor, BitmapProvider provider) {
-    super(processor);
-    this.provider = provider;
-  }
-
-  @Override public boolean canRender(@Nullable AndroidContext context, @Nonnull CDARichNode node) {
-    return node instanceof CDARichEmbeddedInline;
-  }
-
-  @Override protected View inflateRichLayout(@Nonnull AndroidContext context, @Nonnull CDARichNode node) {
-    final View embedded = context.getInflater().inflate(R.layout.rich_embedded_layout, null, false);
-    final ViewGroup content = embedded.findViewById(R.id.rich_content);
-
-    final CDARichEmbeddedInline link = (CDARichEmbeddedInline) node;
-    final Object data = link.getData();
-
-    final View toBeEmbedded;
-    if (data instanceof CDAEntry) {
-      final CDAEntry entry = (CDAEntry) data;
-
-      final TextView textView = new TextView(context.getAndroidContext());
-      textView.setText(entry.getField("title"));
-
-      toBeEmbedded = textView;
-    } else if (data instanceof CDAAsset) {
-      final CDAAsset asset = (CDAAsset) data;
-      final ImageView image = new ImageView(context.getAndroidContext());
-      image.setImageBitmap(provider.provide(context.getAndroidContext(), asset));
-
-      toBeEmbedded = image;
-    } else {
-      final TextView textView = new TextView(context.getAndroidContext());
-      textView.setText("⚠️");
-
-      toBeEmbedded = textView;
+    public EmbeddedLinkRenderer(@Nonnull AndroidProcessor<View> processor) {
+        this(processor, defaultBitmapProvider);
     }
 
-    content.addView(toBeEmbedded, 0);
-    return embedded;
-  }
+    public EmbeddedLinkRenderer(@Nonnull AndroidProcessor<View> processor, BitmapProvider provider) {
+        super(processor);
+        this.provider = provider;
+    }
+
+    @Override
+    public boolean canRender(@Nullable AndroidContext context, @Nonnull CDARichNode node) {
+        return node instanceof CDARichEmbeddedInline;
+    }
+
+    @Nonnull
+    @Override
+    protected View inflateRichLayout(@Nonnull AndroidContext context, @Nonnull CDARichNode node) {
+        final View embedded = context.getInflater().inflate(R.layout.rich_embedded_layout, null, false);
+        final ViewGroup content = embedded.findViewById(R.id.rich_content);
+
+        final CDARichEmbeddedInline link = (CDARichEmbeddedInline) node;
+        final Object data = link.getData();
+
+        final View toBeEmbedded;
+        if (data instanceof CDAEntry) {
+            final CDAEntry entry = (CDAEntry) data;
+
+            final TextView textView = new TextView(context.getAndroidContext());
+            textView.setText(entry.getField("title"));
+
+            toBeEmbedded = textView;
+        } else if (data instanceof CDAAsset) {
+            final CDAAsset asset = (CDAAsset) data;
+            final ImageView image = new ImageView(context.getAndroidContext());
+            image.setImageBitmap(provider.provide(context.getAndroidContext(), asset));
+
+            toBeEmbedded = image;
+        } else {
+            final TextView textView = new TextView(context.getAndroidContext());
+            textView.setText("⚠️");
+
+            toBeEmbedded = textView;
+        }
+
+        content.addView(toBeEmbedded, 0);
+        return embedded;
+    }
 }
